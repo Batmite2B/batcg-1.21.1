a889d2f53d05d00a91cf11fac39e3d269ecb0d5c
Bat <nietzshm@gmail.com>
2026-02-05 03:39:48 -0700

    Initial commit

---
+++ src/main/java/bat/batcg/BatcgClient.java
@@ -0,0 +1,10 @@
+package bat.batcg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BatcgClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
