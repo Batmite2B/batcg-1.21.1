51adb2ab86a30191e329438183a8f92d5431fa92
Bat <nietzshm@gmail.com>
2026-02-05 03:45:24 -0700

    Added Registering

---
+++ src/main/java/bat/batcg/item/ModItems.java
@@ -0,0 +1,12 @@
+package bat.batcg.item;
+
+import bat.batcg.Batcg;
+
+public class ModItems {
+
+    public static void RegisterModItems(){
+        Batcg.LOGGER.info("Registering Mod Items for "+ Batcg.MOD_ID);
+
+    }
+
+}
